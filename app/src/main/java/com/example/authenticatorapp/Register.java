f19ffd9e8923802da6b0a363d6a73c02cd4a9133
Gayan Sachintha <118958174+Gayan-Sachintha@users.noreply.github.com>
2023-12-26 00:23:09 +0530

    Third commit in this commit i used firebase firestore to store users data.

--- app/src/main/java/com/example/authenticatorapp/Register.java
+++ app/src/main/java/com/example/authenticatorapp/Register.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,16 +14,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class Register extends AppCompatActivity {
 
+    public static final String TAG = "TAG";
     EditText mFullName,mEmail,mPassword,mPhone;
     Button mRegisterBtn;
     TextView mLoginBtn;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +48,7 @@ public class Register extends AppCompatActivity {
         mLoginBtn = findViewById(R.id.createText);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
         if(fAuth.getCurrentUser() != null) {
             startActivity(new Intent(getApplicationContext(),MainActivity.class));
@@ -48,6 +60,8 @@ public class Register extends AppCompatActivity {
             public void onClick(View v) {
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
+                String fullName = mFullName.getText().toString();
+                String phone = mPhone.getText().toString();
 
                 if(TextUtils.isEmpty(email)) {
                     mEmail.setError("Email is Required");
@@ -69,6 +83,18 @@ public class Register extends AppCompatActivity {
 
                        if(task.isSuccessful()) {
                            Toast.makeText(Register.this,"User Created.", Toast.LENGTH_SHORT).show();
+                           userID = fAuth.getCurrentUser().getUid();
+                           DocumentReference documentReference = fStore.collection("users").document(userID);
+                           Map<String, Object> user = new HashMap<>();
+                           user.put("fname",fullName);
+                           user.put("email",email);
+                           user.put("phone",phone);
+                           documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                               @Override
+                               public void onSuccess(Void aVoid) {
+                                   Log.d(TAG, "onSuccess: User profile is created for "+ userID);
+                               }
+                           });
                            startActivity(new Intent(getApplicationContext(),Login.class));
                        }else {
                            Toast.makeText(Register.this,"ERROR !" + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
