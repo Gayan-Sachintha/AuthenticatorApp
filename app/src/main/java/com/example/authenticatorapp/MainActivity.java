5e4bebd616ffb306c530646c7f0b17463e5fcdde
Gayan Sachintha <118958174+Gayan-Sachintha@users.noreply.github.com>
2023-12-28 00:49:05 +0530

    Eight commit in this commit i tried to create profile picture updating but not working

--- app/src/main/java/com/example/authenticatorapp/MainActivity.java
+++ app/src/main/java/com/example/authenticatorapp/MainActivity.java
@@ -5,13 +5,17 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,13 +30,14 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int GALLERY_INTENT_CODE = 1023;
     TextView fullName,email,phone,verifyMsg;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userId;
-    Button resendCode,resetPasLocal;
+    Button resendCode,resetPasLocal,changeProfileImage;
     FirebaseUser user;
+    ImageView profileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
         email = findViewById(R.id.profileEmail);
         fullName = findViewById(R.id.profileName);
         resetPasLocal = findViewById(R.id.resetPasswordLocal);
+
+        profileImage = findViewById(R.id.profileImage);
+        changeProfileImage = findViewById(R.id.changeProfile);
 
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
@@ -124,6 +132,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        changeProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //open the gallery
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGalleryIntent, 1000);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000) {
+            if(requestCode == Activity.RESULT_OK) {
+                Uri imageUri = data.getData();
+                profileImage.setImageURI(imageUri);
+            }
+        }
     }
 
     public void logout (View view) {
